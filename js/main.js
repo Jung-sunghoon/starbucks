1c9e18f5779351e3d11d35d20e5979b5f699f834
JShoon <jsh021813@gmail.com>
2023-08-24 16:43:13 +0900

    스타벅스 예제

---
+++ js/main.js
@@ -0,0 +1,52 @@
+//검색 부분 클릭 시 동작
+const searchEl = document.querySelector('.search');
+const searchInputEl = searchEl.querySelector('input');
+
+searchEl.addEventListener('click', function () {
+  searchInputEl.focus();
+});
+
+searchInputEl.addEventListener('focus', function () {
+  searchEl.classList.add('focused');
+  searchInputEl.setAttribute('placeholder', '통합검색');
+});
+
+searchInputEl.addEventListener('blur', function () {
+  searchEl.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder', '');
+});
+
+
+//스크롤에 따라 배지 애니메이션 동작 함수
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function () {
+  console.log(window.scrollY);
+  if(window.scrollY > 500) {
+    //배지 숨기기
+    //gsap.to(요소, 지속시간, 옵션);
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    });
+  }
+  else {
+    //배지 보이기
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    });
+  }
+
+}, 300));
+// _.throttle(함수, 시간(ms))
+
+//fade-in 동작
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fadeEl, index) {
+  //gsap.to(요소, 지속시간, 옵션);
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * .7, //0.7, 1.4, 2.1, 2.8
+    opacity: 1
+  });
+});
